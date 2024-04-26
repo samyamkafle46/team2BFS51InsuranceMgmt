cf79e9f7b507507f25d689ff2409086361567799
manojsingh49 <mmsingh1205@gmail.com>
2024-04-26 12:35:31 -0500

    Updated CustomerInfo file with code changes

---
+++ src/Models/AdminDAO.java
@@ -0,0 +1,10 @@
+package Models;
+
+public interface AdminDAO {
+    void viewUsers();
+    void manageCategories();
+    void manageSubCategories();
+    void managePolices();
+    void manageBuyerPolices();
+
+}
