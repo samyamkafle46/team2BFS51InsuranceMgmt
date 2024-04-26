bfc935eaf5196b57e999228706420d2c3704715e
manojsingh49 <mmsingh1205@gmail.com>
2024-04-26 12:30:51 -0500

    Updated CustomerInfo file with code changes

---
+++ src/Models/CustomerDAOImpl.java
@@ -0,0 +1,31 @@
+package Models;
+
+import java.util.List;
+
+public class CustomerDAOImpl implements CustomerDAO {
+
+    @Override
+    public void addCustomer(CustomerInfo customerInfo) {
+
+    }
+
+    @Override
+    public void updateCustomer(CustomerInfo customerInfo) {
+
+    }
+
+    @Override
+    public void deleteCustomer(CustomerInfo customerInfo) {
+
+    }
+
+    @Override
+    public CustomerInfo getCustomerByID(int customerId) {
+        return null;
+    }
+
+    @Override
+    public List<CustomerInfo> getAllCustomer() {
+        return null;
+    }
+}
