f3b8141a94ffc134ea22237646cc3d9c4b0b27fb
manojsingh49 <mmsingh1205@gmail.com>
2024-04-26 12:27:39 -0500

    Updated CustomerInfo file with code changes

---
+++ src/Models/CustomerDAO.java
@@ -0,0 +1,13 @@
+package Models;
+
+import java.util.List;
+
+interface CustomerDAO {
+    void addCustomer(CustomerInfo customerInfo);
+    void updateCustomer(CustomerInfo customerInfo);
+    void deleteCustomer(CustomerInfo customerInfo);
+
+    CustomerInfo getCustomerByID(int customerId);
+    List<CustomerInfo> getAllCustomer();
+
+}
