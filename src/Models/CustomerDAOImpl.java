29fb883c68fa02d40057e39ac295272afc52fe0b
manojsingh49 <mmsingh1205@gmail.com>
2024-04-29 13:58:47 -0500

    Updated CustomerDAOImpl file with code changes.

--- src/Models/CustomerDAOImpl.java
+++ src/Models/CustomerDAOImpl.java
@@ -1,31 +1,67 @@
 package Models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerDAOImpl implements CustomerDAO {
+    private List<CustomerInfo>customerInfoList = new ArrayList<>();
+
 
     @Override
     public void addCustomer(CustomerInfo customerInfo) {
-
+        customerInfoList.add(customerInfo);
     }
 
     @Override
     public void updateCustomer(CustomerInfo customerInfo) {
+        int index = getCustomerIndexById(customerInfo.getCustomerID());
+        if(index != -1){
+            customerInfoList.set(index, customerInfo);
+        }else{
+            System.out.println("Customer not found");
+        }
+
+        }
+
+
+
+    @Override
+    public void deleteCustomer(CustomerInfo customerInfo) {
+        int index = getCustomerIndexById(customerInfo.getCustomerID());
+        if(index != -1){
+            customerInfoList.remove(index);
+        }else{
+            System.out.println("Customer not found");
+        }
+
+    }
+    @Override
+    public List<CustomerInfo> getAllCustomer() {
+        return new ArrayList<>(customerInfoList);
 
     }
 
     @Override
-    public void deleteCustomer(CustomerInfo customerInfo) {
-
+    public CustomerInfo getCustomerById(boolean customerId) {
+        return null;
     }
 
     @Override
     public CustomerInfo getCustomerByID(int customerId) {
+        int index = getCustomerIndexById(customerId);
+        if (index != -1) {
+                return customerInfoList.get(index);
+            }
         return null;
+    }
+    private int getCustomerIndexById(int customerId) {
+        for (int i = 0; i < customerInfoList.size(); i++) {
+            if (customerInfoList.get(i).getCustomerID() == customerId) {
+                return i;
+            }
+        }
+        return -1;
+    }
     }
 
-    @Override
-    public List<CustomerInfo> getAllCustomer() {
-        return null;
-    }
-}
+
