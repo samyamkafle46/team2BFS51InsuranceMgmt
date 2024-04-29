d6f1f25f43f0987e2efafdbbd11aee2afcb0edba
manojsingh49 <mmsingh1205@gmail.com>
2024-04-29 13:50:36 -0500

    Updated CustomerInfo file with code changes

--- src/Models/CustomerInfo.java
+++ src/Models/CustomerInfo.java
@@ -1,20 +1,29 @@
 package Models;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class CustomerInfo {
     private int customerID;
     private String customerName;
     private String customerPoneNum;
     private String customerEmailAddress;
+    private Set <Category> availableCategory;
+    private Set <SubCategory> availableSubCategory;
+    private List<Policy>availablePolicies;
 
-    public CustomerInfo (int customerID, String customerName, String customerPoneNum,
-                         String customerEmailAddress) {
+
+
+    public CustomerInfo(int customerID, String customerName, String customerPoneNum, String customerEmailAddress) {
         this.customerID = customerID;
         this.customerName = customerName;
         this.customerPoneNum = customerPoneNum;
         this.customerEmailAddress = customerEmailAddress;
     }
+public CustomerInfo(){
 
-
+}
     public int getCustomerID() {
         return customerID;
     }
@@ -46,4 +55,81 @@ public class CustomerInfo {
     public void setCustomerEmailAddress(String customerEmailAddress) {
         this.customerEmailAddress = customerEmailAddress;
     }
+
+    public void viewCategories() {
+        Set<String> categories = new HashSet<>();
+
+        if(availableCategory == null || availableCategory.isEmpty()) {
+            System.out.println("No category available");
+        }else{
+            for (Category category : availableCategory) {
+                int categoryId = category.getCategoryId();
+                String categoryName = category.getCategoryName(categoryId);
+                if (categoryName != null) {
+                    categories.add(categoryName);
+
+                }
+            }
+            System.out.println("Available Policy Categories: ");
+            for(String categoryName : categories){
+                System.out.println("-"+ categoryName);
+            }
+        }
+
+    }
+
+    public void viewSubCategories() {
+        Set<String> subCategories = new HashSet<>();
+        if (availableSubCategory == null || availableSubCategory.isEmpty()) {
+            System.out.println("No Subcategory available");
+
+        } else {
+            for (SubCategory subcategory : availableSubCategory) {
+                int subcategoryId = subcategory.getSubCategoryId();
+                String subcategoryName = subcategory.getSubCategoryName(subcategoryId);
+                if (subcategoryName != null) {
+                    subCategories.add(subcategoryName);
+                }
+            }
+            System.out.println("Available  sub categories: ");
+            for (String subcategoryName : subCategories) {
+                System.out.println("-" + subcategoryName);
+
+            }
+        }
+    }
+    public void viewPolicies() {
+        if(availablePolicies == null || availablePolicies.isEmpty()){
+            System.out.println("No policies available");
+        }else{
+            System.out.println("Available Polices:");
+            for(Policy policy : availablePolicies){
+                System.out.println("-"+policy.getPolicyName());
+            }
+        }
+    }
+
+    public void applyForPolicy(Policy policy) {
+        if(availablePolicies == null || availablePolicies.isEmpty()){
+            System.out.println("No policies available to apply for.");
+            return;
+        }
+        if(availablePolicies.contains(policy)){
+            System.out.println("Applying for policy: ");
+            boolean applicationSubmitted = submitPolicyApplication(policy);
+            if(applicationSubmitted){
+                System.out.println("Policy application submitted successfully.");
+            }else {
+                System.out.println("An error occurred while submitting the policy application.");
+            }
+
+        }else{
+            System.out.println("The selected policy is not available.");
+        }
+    }
+    private boolean submitPolicyApplication(Policy policy) {
+        return true;
+
+    }
 }
+
