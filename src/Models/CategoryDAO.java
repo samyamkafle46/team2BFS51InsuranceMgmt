82dae5cb8a9af7f37de9ff02bff06a0de50f8796
Dinishan1997 <166424184+Dinishan1997@users.noreply.github.com>
2024-04-30 22:47:18 -0400

    Update CategoryDAO.java

--- src/Models/CategoryDAO.java
+++ src/Models/CategoryDAO.java
@@ -1,4 +1,8 @@
 package Models;
 
 public interface CategoryDAO {
+    void saveCategory(Category category);
+    Category getCategoryByName(String categoryName);
+    void updateCategory(Category category);
+    void deleteCategory(String categoryName);
 }
