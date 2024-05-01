4fc2daaed1160334b959565e0925ee6b3111a66b
Dinishan1997 <166424184+Dinishan1997@users.noreply.github.com>
2024-04-30 22:13:41 -0400

    Update SubCategoryDAO.java

--- src/Models/SubCategoryDAO.java
+++ src/Models/SubCategoryDAO.java
@@ -1,4 +1,10 @@
 package Models;
 
 public class SubCategoryDAO {
+	
+	List<SubCategory> getAllSubCategories();
+    SubCategory getSubCategoryById(int subCategoryId);
+    void addSubCategory(SubCategory subCategory);
+    void updateSubCategory(SubCategory subCategory);
+    void deleteSubCategory(int subCategoryId);
 }
