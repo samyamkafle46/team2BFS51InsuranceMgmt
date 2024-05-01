873398bbf6ba02a3737d49ff2dd8e4f91984645c
Dinishan1997 <166424184+Dinishan1997@users.noreply.github.com>
2024-04-30 22:46:36 -0400

    Update Category.java

--- src/Models/Category.java
+++ src/Models/Category.java
@@ -1,4 +1,62 @@
 package Models;
 
 public class Category {
+
+	private String categoryID;
+	private String categoryName;
+	private String categoryType;
+	private String categoryDescription;
+	
+	public Category (String categoryID,  String categoryName, String categoryType, String categoryDescription) {
+		
+		this.categoryID = categoryID;
+		this.categoryName = categoryName;
+		this.categoryType = categoryType;
+		this.categoryDescription = categoryDescription;
+		
+	}
+	
+	 public String getCategoryID() {
+	        return categoryID;
+	    }
+
+	    public void setCategoryID(String categoryID) {
+	        this.categoryID = categoryID;
+	    }
+
+	
+	 public String getCategoryName() {
+	        return categoryName;
+	    }
+
+	    public void setCategoryName(String categoryName) {
+	        this.categoryName = categoryName;
+	    }
+
+	    public String getCategoryType() {
+	        return categoryType;
+	    }
+
+	    public void setCategoryType(String categoryType) {
+	        this.categoryType = categoryType;
+	    }
+
+	    public String getCategoryDescription() {
+	        return categoryDescription;
+	    }
+
+	    public void setCategoryDescription(String categoryDescription) {
+	        this.categoryDescription = categoryDescription;
+	    }
+	    
+	    public String toString() {
+	        return "Category{" +
+	        		"categoryID='" + categoryID + '\'' +
+	                "categoryName='" + categoryName + '\'' +
+	                ", categoryType='" + categoryType + '\'' +
+	                ", categoryDescription='" + categoryDescription + '\'' +
+	                '}';
+	    }
+
+
 }
